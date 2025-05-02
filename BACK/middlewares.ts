94072a9c43cd9f9cd962d1808f3583b4c2782dfe
yanis.niaussat <yanis.niaussat@etu.umontpellier.fr>
2025-05-02 02:17:39 +0200

    refactor : better auth middleware.

--- BACK/middlewares.ts
+++ BACK/middlewares.ts
@@ -3,27 +3,25 @@ import {verifyJWT,createJWT} from "./jwt_func.ts"
 
 
 async function authorizationMiddleware(ctx: Context, next: () => Promise<unknown>) {
-    /*const authHeader = ctx.request.headers.get("Authorization");
-    console.log(authHeader);
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      ctx.response.status = 401;
-      ctx.response.body = { message: "Unauthorized: Missing or invalid Authorization header" };
-      return;
-    }
-  
-    let token = authHeader.substring(7);
-    */
-    const token = String(ctx.request.url.searchParams.get('token'));
+    const token = await ctx.cookies.get('refreshToken');
+    //console.log(token);
+    if (token){
+      const payload = await verifyJWT(token!);
     
-    const payload = await verifyJWT(token);
-  
-    if (payload) {
-      console.log("Token verified:", payload);
-      await next(); // Proceed to the next middleware or route handler
-    } else {
-      console.error("Token verification failed");
+      if (payload) {
+        //console.log("Token verified:", payload);
+        ctx.response.body = {user : payload.username};
+        await next(); // Proceed to the next middleware or route handler
+      } else {
+        console.error("Token verification failed");
+        ctx.response.status = 401;
+        ctx.response.body = { data : "Unauthorized: Invalid token" };
+        return;
+      }
+    }else{
+      console.error("No Token Provided");
       ctx.response.status = 401;
-      ctx.response.body = { message: "Unauthorized: Invalid token" };
+      ctx.response.body = { data : "Unauthorized: No token provided" };
       return;
     }
 }
